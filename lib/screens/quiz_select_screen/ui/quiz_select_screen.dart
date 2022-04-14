9ba35d40d38b93211cef0fa99d2634d0d9b47733
mateuszfilipek2000 <mateuszfilipek2000@gmail.com>
2022-04-14 09:41:16 +0200

    added basic animated background

--- lib/screens/quiz_select_screen/ui/quiz_select_screen.dart
+++ lib/screens/quiz_select_screen/ui/quiz_select_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc_quiz_app/screens/quiz_details_screen/ui/quiz_detail
 import 'package:flutter_bloc_quiz_app/screens/quiz_select_screen/bloc/quiz_select_bloc.dart';
 import 'package:flutter_bloc_quiz_app/screens/quiz_select_screen/bloc/quiz_select_event.dart';
 import 'package:flutter_bloc_quiz_app/screens/quiz_select_screen/bloc/quiz_select_state.dart';
+import 'dart:math';
 
 class QuizSelectScreen extends StatelessWidget {
   static const String routeName = '/';
@@ -25,18 +26,19 @@ class QuizSelectScreen extends StatelessWidget {
           }
           if (state is QuizSelectionFailure) {
             showDialog(
-                context: context,
-                builder: (_) {
-                  return AlertDialog(
-                    title: const Text("Quiz selection error"),
-                    actions: [
-                      TextButton(
-                        child: const Text("Ok"),
-                        onPressed: () => Navigator.pop(context),
-                      ),
-                    ],
-                  );
-                });
+              context: context,
+              builder: (_) {
+                return AlertDialog(
+                  title: const Text("Quiz selection error"),
+                  actions: [
+                    TextButton(
+                      child: const Text("Ok"),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ],
+                );
+              },
+            );
           }
         },
         child: Stack(
@@ -85,9 +87,122 @@ class AnimatedBackground extends StatefulWidget {
   State<AnimatedBackground> createState() => _AnimatedBackgroundState();
 }
 
-class _AnimatedBackgroundState extends State<AnimatedBackground> {
+class _AnimatedBackgroundState extends State<AnimatedBackground>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 10))
+          ..repeat();
+
+    animation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(animationController);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final double width = MediaQuery.of(context).size.width;
+    return AnimatedWaves(animation: animation, width: width);
   }
+}
+
+class AnimatedWaves extends AnimatedWidget {
+  const AnimatedWaves({Key? key, required this.animation, required this.width})
+      : super(listenable: animation);
+
+  final Animation<double> animation;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipPath(
+          clipper: WaveClipper(0.11, animation.value, offsetPercent: 0.2),
+          child: Container(
+            color: const Color(0xFFFFDDE5),
+          ),
+        ),
+        ClipPath(
+          clipper: WaveClipper(0.1, animation.value),
+          child: Container(
+            color: const Color(0xFFEE4266),
+          ),
+        ),
+        ClipPath(
+          clipper: WaveClipper(0.08, animation.value, offsetPercent: 0.2),
+          child: Container(
+            color: const Color(0xFFFFB8C8),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  WaveClipper(this.heightFraction, this.animationValue,
+      {this.offsetPercent = 0});
+
+  double heightFraction;
+  double animationValue;
+  double offsetPercent;
+
+  @override
+  Path getClip(Size size) {
+    var width = size.width;
+    var height = size.height;
+
+    var waveHeight = height - (height * heightFraction);
+
+    var percentOfHeight = height / 100;
+    var percentOfWidth = width / 100;
+
+    Path path = Path()
+      ..moveTo(width, waveHeight)
+      ..lineTo(width, height)
+      ..lineTo(0, height)
+      ..lineTo(0, waveHeight);
+
+    for (int x = 0; x <= width; x++) {
+      // current width fraction (0-1)
+      var xPos = x / width * 2 * pi;
+      var currentWidthShift =
+          (animationValue * 2 * pi) + (width * offsetPercent);
+
+      // currentWidthShift *= speed;
+      path.lineTo(
+          x.toDouble(), waveHeight + sin(2 * pi + xPos + currentWidthShift) * 50
+          // waveHeight +
+          //     sin(((size.width * offsetPercent + x) / size.width * 4 * pi) +
+          //             ((speed * animationValue) * 2 * pi)) *
+          //         30,
+          );
+    }
+
+    // Path path = Path()
+    //   ..moveTo(width + widthShift, waveHeight)
+    //   ..lineTo(width + widthShift, height)
+    //   ..lineTo(0, height)
+    //   ..lineTo(0, waveHeight)
+    //   ..cubicTo(p1.dx, p1.dy, p2.dx, p2.dy, width + widthShift, waveHeight)
+    //   ..close();
+    return path..close();
+  }
+
+  @override
+  bool shouldReclip(WaveClipper oldClipper) =>
+      heightFraction != oldClipper.heightFraction ||
+      animationValue != oldClipper.animationValue;
 }
